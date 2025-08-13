fd52b300d449fe4062143be441978b61a9b467db
Le big Zakaria <zakghb@hotmail.com>
2025-08-13 18:41:08 +0200

    philo

---
+++ philo/philo.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-ghab <zel-ghab@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 18:50:37 by zel-ghab          #+#    #+#             */
+/*   Updated: 2025/08/13 15:22:53 by zel-ghab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	philosophers(char **argv)
+{
+	t_simulation	*simulation;
+	int		i;
+
+	i = 0;
+	if (parsing(argv) == 1)
+		return (1); 
+	simulation = malloc(sizeof(t_simulation));
+	if (!simulation)
+		return (1);
+	initialisation(&simulation, argv);
+	while (simulation->philo[i])
+	{
+		if (pthread_join(simulation->philo[i]->thread, NULL) != 0)
+			return (1);
+		i++;
+	}
+	if (pthread_join(simulation->doctor, NULL) != 0)
+		return (1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 5 || argc == 6)
+		philosophers(argv);
+	else
+		return (printf("Error\n"), 1);
+	return (0);
+}
---
+++ philo/philo.h
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-ghab <zel-ghab@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 18:50:46 by zel-ghab          #+#    #+#             */
+/*   Updated: 2025/08/13 15:36:53 by zel-ghab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+typedef struct s_simulation
+{
+	int		philos;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		time_to_think;
+	long		start_time;
+	int		nb_meals;
+	int		global_meals;
+	int		dead;
+	struct s_philo	**philo;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	*print;
+	pthread_mutex_t	*dead_mutex;
+	pthread_mutex_t	*last_meal;
+	pthread_mutex_t	*count_meal;
+	pthread_t	doctor;
+}	t_simulation;
+
+typedef struct s_philo
+{
+	pthread_t		thread;
+	int			id;
+	int			meals;
+	long			last_meal;
+	struct s_simulation	*simulation;
+	pthread_mutex_t		*right_fork;
+	pthread_mutex_t		*left_fork;
+}	t_philo;
+
+/*----------------  philo.c  ---------------*/
+int	philosophers(char **argv);
+int	main(int argc, char **argv);
+
+/*----------------  parsing.c  ---------------*/
+int	ft_atoi(char *str);
+int	check_numbers(char **argv);
+int	parsing(char **argv);
+
+/*----------------  initialisation.c  ---------------*/
+t_philo		*init_philo(int id, t_simulation **simulation);
+void		init_mutex(t_simulation **simulation);
+void		init_simulation(t_simulation **simulation, char **argv);
+void		give_fork(t_simulation **simulation);
+void	initialisation(t_simulation **simulation, char **argv);
+
+/*----------------  routine.c  ---------------*/
+void	*thread_routine(void *arg);
+void	is_sleeping(t_philo *philo);
+void	is_eating(t_philo *philo);
+void	is_thinking(t_philo *philo);
+long	get_time_ms(void);
+
+/*----------------  doctor.c  ---------------*/
+void	*thread_doctor(void *arg);
+
+#endif
